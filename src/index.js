a73caf2a8c99a0314980cf6f9cefb5f1f9143aa5
macandelachaves <149009525+macandelachaves@users.noreply.github.com>
2024-04-06 16:58:48 +0100

    UX updates

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+function displayPoem(response) {
+  console.log("poem generated");
+  new Typewriter("#main-poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 30,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
+  let poemRequest = document.querySelector("#poem-request");
+  let apiKey = "36bb04605e1o2a2f7ce6c8t6fa5fe0ba";
+  let context =
+    " you are a romantic poem expert and love to write short poems in spanish. Your mission is to generate a 4 line poem and separte each line with a <br>. Make sure to follow the use instructions. Sign the poem with the author name inside a <strong> element at the end of the poem. Show just the name of the author, without other words. The poem has to be in spanish ";
+  let prompt = `Generate a spanish poem about${poemRequest}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  new Typewriter("#main-poem", {
-    strings: `Vivir la vida y aceptar el reto,\rrecuperar la risa, ensayar el canto, \rbajar la guardia y extender las manos, \rdesplegar las alas e intentar de nuevo, \rcelebrar la vida y retomar los cielos `,
-    autoStart: true,
-    Delay: 1,
-    cursor: "",
-  });
+  let mainPoem = document.querySelector("#main-poem");
+  mainPoem.classList.remove("hidden");
+  mainPoem.innerHTML = ` <div class="generating"> Generating the requested poem about ${poemRequest.value} </div>`;
+
+  axios.get(apiUrl).then(displayPoem);
+
+  console.log("Generating poem");
 }
 
 let mainPoem = document.querySelector("#poem-form");
